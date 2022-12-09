2d01b903711aecb1c8b28107943787d4e9ce3e3b
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-12-08 19:16:32 -0800

    🗃️ Add dataframe

--- app.R
+++ app.R
@@ -1,7 +1,13 @@
 library(shiny)
+library(tidyverse)
+
+datos <- read_csv("https://raw.githubusercontent.com/nepito/world_cup_semis/develop/tests/data/morocco_vs_spain.csv")
 ui <- fluidPage(
-  "Un saludo a familia bonita del NIES"
+  tableOutput("static"),
+  dataTableOutput("dynamic")
 )
 server <- function(input, output, session) {
+  output$static <- renderTable(head(datos))
+  output$dynamic <- renderDataTable(datos, options = list(pageLength = 5))
 }
 shinyApp(ui, server)
