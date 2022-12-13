dd7ad5c6b031d15209bfeb69cfd5af40f9d41624
davidmacer <jdavid.macer@gmail.com>
2022-12-13 15:34:31 -0600

    Quita la tabla estática y pon una gráfica de la suma de goles

--- app.R
+++ app.R
@@ -4,11 +4,13 @@ library(tidyverse)
 datos <- read_csv("https://raw.githubusercontent.com/nepito/world_cup_semis/develop/tests/data/morocco_vs_spain.csv")
 ui <- fluidPage(
   titlePanel(title = span(img(src = "logo.jpeg", height = 35), "")),
-  tableOutput("static"),
+  plotOutput("static"),
   dataTableOutput("dynamic")
 )
 server <- function(input, output, session) {
-  output$static <- renderTable(head(datos))
+  p <- datos %>% group_by(Team) %>% summarise(suma_goles = sum(Gls, na.rm = T)) %>% 
+    ggplot() + geom_col(aes(Team, suma_goles))
+  output$static <- renderPlot(p)
   output$dynamic <- renderDataTable(datos, options = list(pageLength = 5))
 }
 shinyApp(ui, server)
